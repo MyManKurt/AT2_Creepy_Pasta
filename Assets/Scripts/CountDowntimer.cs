a5ecf21a152fdc5daa1109172d963070a7005055
MyManKurt <Kurtcpriv@gmail.com>
2024-09-16 12:09:31 +0800

    Alpha 1.1
    Some changes

---
+++ Assets/Scripts/CountDowntimer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+//UI
+
+public class CountDowntimer : MonoBehaviour
+{
+    /*
+    Timer starts count down at the start of the game
+    Player must find win location before timer hits 0
+    Else player loses game
+
+    //bool - True/False
+    //string - +{Pk093?!@"'
+    //float - 0.0
+    //int - whole number 10 -52 1067
+     */
+    //Countdown timer - float
+
+    public float countDownTimer = 150;
+    [SerializeField]
+    public UnityEvent timerEnd = new UnityEvent();
+    //Text
+
+    private void Start()
+    {
+        countDownTimer = 150;
+    }
+    private void Update()
+    {
+        CountDown();
+        //Display Timer
+    }
+    void CountDown()
+    {
+        if (countDownTimer > 0)
+        {
+            countDownTimer -= Time.deltaTime;
+            if (countDownTimer <= 0)
+            {
+                countDownTimer = 0;
+                timerEnd.Invoke();
+            }
+        }
+       
+    }
+}
