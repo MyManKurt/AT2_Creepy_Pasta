b9b9cf2e1366310db40ced612b60479eee0a7ba9
MyManKurt <Kurtcpriv@gmail.com>
2024-09-16 08:30:13 +0800

    Alpha 1.01
    Ceilings redone, starting room done up and final room note added

---
+++ Assets/Scripts/Player/TorchHandler.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchHandler : MonoBehaviour
+{
+    public GameObject torch;
+    public Light bright;
+    public float power = 1;
+    public bool isOn;
+    private void Awake()
+    {
+        torch.SetActive(false);
+    }
+    public void Pickup()
+    {
+        torch.SetActive(true);
+        isOn = true;
+    }
+    private void Update()
+    {
+        if (isOn && power > 0)
+        {
+            power -= 0.05f * Time.deltaTime;
+            bright.intensity = Mathf.Clamp01(power);
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isOn = !isOn;
+            bright.enabled = isOn;
+        }
+    }
+
+
+
+}
