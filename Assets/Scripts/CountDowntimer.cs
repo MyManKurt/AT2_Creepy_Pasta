09d80b6ac3cfba452140a433f25721ddf4a85e89
MyManKurt <Kurtcpriv@gmail.com>
2024-10-07 08:12:57 +0800

    Added Copywrite to Custom Written Sript

--- Assets/Scripts/CountDowntimer.cs
+++ Assets/Scripts/CountDowntimer.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
 
+#region AUTHOR & COPYRIGHT DETAILS
+
+/// Original Author: Kurtis Jackson
+/// Contact: Kurtis Jackson <Kurtcprive@gmail.com>.
+/// Contributing Authors: 
+/// Last Updated: October, 2024
+/// 
+/// ###---**COPYRIGHT STATEMENT**---###
+/// © Copyright 2024 South Metropolitan TAFE. All rights reserved.
+/// This code is provided by student of South Metropolitan TAFE for educational purposes only.
+/// Unauthorized use, including but not limited to sharing, redistributing, copying, or commercialising
+/// this code or any part of it, without the express written permission of the authors, is strictly prohibited.
+/// </remarks>
+#endregion
+
 
 public class CountDowntimer : MonoBehaviour
 {
@@ -19,21 +34,32 @@ public class CountDowntimer : MonoBehaviour
      */
     //Countdown timer - float
 
+    //Countdown timer starts at 215 seconds and counts down to 0
+    //on timer end trigger game LoseState
     public float countDownTimer = 215;
     [SerializeField]
     public UnityEvent timerEnd = new UnityEvent();
     public Text timerDisplay;
 
+    /// <summary>
+    /// sets timer to 215 seconds
+    /// </summary>
     private void Start()
     {
         countDownTimer = 215;
         timerDisplay = GameObject.FindGameObjectWithTag("CountDown").GetComponent<Text>();
     }
+    /// <summary>
+    /// sets how the timer format should display on screen to the player
+    /// </summary>
     private void Update()
     {
         CountDown();
         timerDisplay.text = string.Format("{0:0.00}", countDownTimer);
     }
+    /// <summary>
+    /// if timer hits 0 the game ends
+    /// </summary>
     void CountDown()
     {
         if (countDownTimer > 0)
